6db345f6bf1407c8f62a559921b16a19981a59df
new_dragon <tkddyd1126@gmail.com>
2024-11-07 18:30:28 +0900

    test(PromotionTest.java): Promotion.currentPromotion함수 테스트
    - 프로모션 기간인지 확인하는 테스트 작성

---
+++ src/test/java/model/PromotionTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.LocalDateUtil;
+
+class PromotionTest {
+    @Test
+    @DisplayName("오늘이 프로모션 기간일때 확인하기")
+    public void 프로모션_기간일때_테스트() {
+        LocalDate startTime = LocalDateUtil.parse("2024-11-06");
+        LocalDate endTime = LocalDateUtil.parse("2024-11-08");
+        assertThat(Promotion.currentPromotion(startTime, endTime)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("오늘이 프로모션 기간 아닐때 확인하기")
+    public void 프로모션_기간_아닐때_테스트() {
+        LocalDate startTime = LocalDateUtil.parse("2023-11-05");
+        LocalDate endTime = LocalDateUtil.parse("2024-11-06");
+        assertThat(Promotion.currentPromotion(startTime, endTime)).isEqualTo(false);
+    }
+}
