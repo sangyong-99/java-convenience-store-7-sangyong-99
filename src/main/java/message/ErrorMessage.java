5e2339195e8134ef9f5e998fcc3c0652a1de1e12
new_dragon <tkddyd1126@gmail.com>
2024-11-08 21:38:57 +0900

    feat(WrongInputException, ErrorMessage.java): 잘못된 입력 예외 구현

--- src/main/java/message/ErrorMessage.java
+++ src/main/java/message/ErrorMessage.java
@@ -1,7 +1,8 @@
 package message;
 
 public enum ErrorMessage {
-    LOAD_FILE_FAIL_MESSAGE("[ERROR] 파일을 제대로 읽어오지 못했습니다.");
+    LOAD_FILE_FAIL_MESSAGE("[ERROR] 파일을 제대로 읽어오지 못했습니다."),
+    WRONG_INPUT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
 
     private final String errorMessage;
 
