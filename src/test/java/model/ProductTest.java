5351b147af59b0e89df0634d8690fdcf88cdac48
new_dragon <tkddyd1126@gmail.com>
2024-11-08 17:00:13 +0900

    test(ProductTest): Product클래스 setPromotionProduct,setCommonProduct함수 테스트 작성
    - Product setPromotionProduct 함수 테스트
    - Product setCommonProduct 함수 테스트

---
+++ src/test/java/model/ProductTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.LocalDateUtil;
+
+class ProductTest {
+    private Product product;
+    private Promotion promotion;
+
+    @BeforeEach
+    public void setUp() {
+        promotion = new Promotion("Holiday Sale", 1, 1, LocalDateUtil.parse("2024-11-01"),
+                LocalDateUtil.parse("2024-11-30"));
+        product = new Product("Laptop", 1000, 5, 0, null);
+    }
+
+    @Test
+    @DisplayName("PromotionProduct 설정 테스트")
+    public void testSetPromotionProduct() {
+        String expectValue = "- Laptop 1,000원 3개 Holiday Sale\n" + "- Laptop 1,000원 5개";
+        product.setPromotionProduct(3, promotion);
+        assertThat(product.toString()).isEqualTo(expectValue);
+    }
+
+    @Test
+    @DisplayName("CommonProduct 설정 테스트")
+    public void testSetCommonProduct() {
+        String expectValue = "- Laptop 1,000원 10개";
+        product.setCommonProduct(10);
+        assertThat(product.toString()).contains(expectValue);
+    }
+}
