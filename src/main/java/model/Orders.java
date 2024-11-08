10babc4afa1d385650e3861c6404805b062487ea
new_dragon <tkddyd1126@gmail.com>
2024-11-08 22:26:54 +0900

    feat(Orders.java): Orders클래스 addOrder, getOrders, clearOrders 함수 구현

--- src/main/java/model/Orders.java
+++ src/main/java/model/Orders.java
@@ -5,4 +5,16 @@ import java.util.List;
 
 public final class Orders {
     private static final List<Order> orders = new ArrayList<>();
+
+    public static void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public static List<Order> getOrders() {
+        return orders;
+    }
+
+    public static void clearOrders() {
+        orders.clear();
+    }
 }
