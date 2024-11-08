8489e5dc9d2f51e637ee13bc994556cd4b9ee131
new_dragon <tkddyd1126@gmail.com>
2024-11-08 23:23:12 +0900

    test(RegexCheckerUtilTest.java): RegexCheckerUtil 클래스 테스트 작성
    - RegexCheckerUtil.orderRegexChecker 함수 테스트 작성

---
+++ src/test/java/util/RegexCheckerUtilTest.java
@@ -0,0 +1,17 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RegexCheckerUtilTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"[감자칩-q]", "사이다-2]", "[사이다-1", "[-1]", "[사이다-0]", "ㅂ[사이다-12]", "[사이다-2]q"})
+    @DisplayName("입력값 형식 예외 발생 확인하기")
+    public void 입력값_정규식_예외_테스트(String input) {
+        assertThatThrownBy(() -> RegexCheckerUtil.orderRegexChecker(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
