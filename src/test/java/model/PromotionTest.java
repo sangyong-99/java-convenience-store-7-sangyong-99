f61dbf612416f835b071686777821f342fc111ad
new_dragon <tkddyd1126@gmail.com>
2024-11-09 00:32:33 +0900

    test(PromotionTest.java): 테스트 코드 날짜 변경

--- src/test/java/model/PromotionTest.java
+++ src/test/java/model/PromotionTest.java
@@ -12,7 +12,7 @@ class PromotionTest {
     @DisplayName("오늘이 프로모션 기간일때 확인하기")
     public void 프로모션_기간일때_테스트() {
         LocalDate startTime = LocalDateUtil.parse("2024-11-06");
-        LocalDate endTime = LocalDateUtil.parse("2024-11-08");
+        LocalDate endTime = LocalDateUtil.parse("2024-12-30");
         assertThat(Promotion.currentPromotion(startTime, endTime)).isEqualTo(true);
     }
 
