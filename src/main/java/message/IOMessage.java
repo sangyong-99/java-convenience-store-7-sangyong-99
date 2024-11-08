c6ba61f3485ccdf54b97a5aaca311c0017ee4e85
new_dragon <tkddyd1126@gmail.com>
2024-11-08 21:26:34 +0900

    fix(IOMessage.java): 사용자 주문 메시지 수정

--- src/main/java/message/IOMessage.java
+++ src/main/java/message/IOMessage.java
@@ -2,7 +2,7 @@ package message;
 
 public enum IOMessage {
     INVENTORY_STATUS_MESSAGE("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n"),
-    ORDER_MESSAGE("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n");
+    ORDER_MESSAGE("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
 
     private final String ioMessage;
 
