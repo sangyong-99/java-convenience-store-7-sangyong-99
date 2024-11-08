334cdfc99faf3320646956a89eb2e85a930d0f0b
new_dragon <tkddyd1126@gmail.com>
2024-11-08 22:27:29 +0900

    test(OrdersTest.java): Orders 함수 테스트 작성

---
+++ src/test/java/model/OrdersTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrdersTest {
+    Order order1 = new Order("콜라", 3);
+    Order order2 = new Order("사이다", 5);
+
+    @Test
+    @DisplayName("Orders.add 함수 확인하기")
+    public void orders_추가_테스트() {
+        Orders.addOrder(order1);
+        Orders.addOrder(order2);
+        assertThat(Orders.getOrders().size()).isEqualTo(2);
+        assertThat(Orders.getOrders().get(0)).isEqualTo(order1);
+        assertThat(Orders.getOrders().get(1)).isEqualTo(order2);
+    }
+}
