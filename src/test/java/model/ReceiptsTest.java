d43be8a3b4048f719cd3f7eb6f12b4f4b82ec26a
new_dragon <tkddyd1126@gmail.com>
2024-11-10 20:12:59 +0900

    test(ReceiptsTest.java): Receipts 클래스 membershipDiscountEligiblePrice 함수 테스트
    - Receipts.membershipDiscountEligiblePrice 멤버십 할인 대상 금액 함수 테스트

--- src/test/java/model/ReceiptsTest.java
+++ src/test/java/model/ReceiptsTest.java
@@ -1,13 +1,30 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ReceiptsTest {
+    Purchase purchase = new Purchase("콜라", 6, 1, 1500, 3);
+
+    @BeforeEach
+    public void clear() {
+        Receipts.clear();
+    }
+
     @Test
     @DisplayName("영수증에 구매 목록 추가 확인하기")
     public void 영수증_구매목록_추가_테스트() {
-        Purchase purchase = new Purchase("콜라", 2, 0, 1500, 0);
         Receipts.add(purchase);
+        assertThat(Receipts.getPurchases().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("멤버십 할인 가격 구하는 함수 확인하기")
+    public void 멤버십_할인가격_함수_테스트() {
+        Receipts.add(purchase);
+        assertThat(Receipts.membershipDiscountEligiblePrice()).isEqualTo(4500);
     }
 }
