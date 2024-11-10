cac7d5f95950e48ceec5bb72d64bda54c6155502
new_dragon <tkddyd1126@gmail.com>
2024-11-10 12:33:46 +0900

    feat(Output.java): String Format 출력 함수 구현

--- src/main/java/io/Output.java
+++ src/main/java/io/Output.java
@@ -4,4 +4,8 @@ public final class Output {
     public static void printlnMessage(final String message) {
         System.out.println(message);
     }
+
+    public static void printlnFormatMessage(final String format, Object... args) {
+        System.out.printf((format) + "%n", args);
+    }
 }
